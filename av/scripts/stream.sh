295e6406dd24e2436c738abed5b8f812f2a896c9
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2020-09-05 00:08:56 +0100

    Change raspivid path for Alpine
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- av/scripts/stream.sh
+++ av/scripts/stream.sh
@@ -2,7 +2,7 @@
 
 while [ True ];
 do
-  raspivid -o - -t 0 -vf -hf -fps 30 -b 6000000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/${STREAM_KEY}
+  /opt/vc/bin/raspivid -o - -t 0 -vf -hf -fps 30 -b 6000000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/${STREAM_KEY}
   sleep 10
 done
 
