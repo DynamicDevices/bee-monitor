a8ef91dad813bab0d5cad9641484211c3d14255b
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2020-09-08 16:48:33 +0100

    Update stream.sh to handle audio a bit better
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- av/scripts/stream.sh
+++ av/scripts/stream.sh
@@ -2,7 +2,8 @@
 
 while [ True ];
 do
-  /opt/vc/bin/raspivid -o - -t 0 -vf -hf -fps 30 -b 6000000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/${STREAM_KEY}
+#  /opt/vc/bin/raspivid -o - -t 0 -vf -hf -fps 30 -b 6000000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/${STREAM_KEY}
+/opt/vc/bin/raspivid -o - -t 0 -w 1280 -h 720 -vf -fps 25 -b 6000000 | ffmpeg -f alsa -thread_queue_size 1024 -ac 1 -i hw:1 -f h264 -i - -vcodec copy -c:a aac -ar 44100 -b:a 128k -f flv rtmp://a.rtmp.youtube.com/live2/${STREAM_KEY}
   sleep 10
 done
 
