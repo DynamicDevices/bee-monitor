38f7cff4380ec2a545d63487b05d7536ff1368b5
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2020-10-09 13:40:45 +0100

    Add gesture detection
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- sensors/scripts/readandpublish.py
+++ sensors/scripts/readandpublish.py
@@ -86,9 +86,19 @@ cpu_temps = []
 from apds9960.const import *
 from apds9960 import APDS9960
 
+dirs = {
+    APDS9960_DIR_NONE: "none",
+    APDS9960_DIR_LEFT: "left",
+    APDS9960_DIR_RIGHT: "right",
+    APDS9960_DIR_UP: "up",
+    APDS9960_DIR_DOWN: "down",
+    APDS9960_DIR_NEAR: "near",
+    APDS9960_DIR_FAR: "far",
+}
 # Initialise the ADPS-9960
 apds = APDS9960(bus)
 apds.enableLightSensor()
+apds.enableGestureSensor()
 
 while True:
 	# Connect / Reconnect up MQTT
@@ -127,6 +137,13 @@ while True:
 	client.publish(MQTT_TOPIC_PREFIX_STATE + "red", r);
 	client.publish(MQTT_TOPIC_PREFIX_STATE + "green", g);
 	client.publish(MQTT_TOPIC_PREFIX_STATE + "blue", b);
+
+	# Not entirely sure what use this is in the context of bees but why not!
+	if apds.isGestureAvailable():
+		motion = apds.readGesture()
+		gesture = dirs.get(motion, "unknown")
+		print("Gesture={}".format(gesture))
+		client.publish(MQTT_TOPIC_PREFIX_STATE + "gesture", gesture);
 
 	# Process MQTT messages
 	client.loop();
