afc11eefc87af88dfd9fb3b84eceeed2ea7b3f7f
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2020-12-10 13:31:57 +0000

    Remove vertical flip
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- av/scripts/stream.sh
+++ av/scripts/stream.sh
@@ -10,7 +10,7 @@ amixer -c 1 set Mic 90%
 # FFMPEG
 
 #  /opt/vc/bin/raspivid -o - -t 0 -vf -hf -fps 30 -b 6000000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/${STREAM_KEY}
-/opt/vc/bin/raspivid -o - -t 0 -w 1280 -h 720 -vf -fps 25 -b 6000000 | ffmpeg -f alsa -thread_queue_size 1024 -ac 1 -i hw:1 -f h264 -i - -vcodec copy -c:a aac -ar 44100 -b:a 128k -f flv rtmp://a.rtmp.youtube.com/live2/${STREAM_KEY}
+/opt/vc/bin/raspivid -o - -t 0 -w 1280 -h 720 -fps 25 -b 6000000 | ffmpeg -f alsa -thread_queue_size 1024 -ac 1 -i hw:1 -f h264 -i - -vcodec copy -c:a aac -ar 44100 -b:a 128k -f flv rtmp://a.rtmp.youtube.com/live2/${STREAM_KEY}
   sleep 10
 
 # GStreamer1.0
